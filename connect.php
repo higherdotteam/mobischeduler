34a60eeb6c9afedf5bd9d4d88903f4647a6ae33b
Christian Momdjian <chrismomdjian@gmail.com>
2017-01-24 12:45:16 -0800

    Add files via upload
    1.Changed handle_datetime.php to insert student id into database.
    2.Changed connect.php to include hdt database connect info rather than my localhost info.

--- connect.php
+++ connect.php
@@ -1,12 +1,5 @@
 <?php
+$conn = mysqli_connect("138.197.209.226", "root", "root") or die("Could not connect");
 
-//SQL login info
-$host = "138.197.209.226";
-$username = "root";
-$password = "root";
-$database = "hdt";
-
-$conn = @mysqli_connect($host, $username, $password) or die("Could not connect.");
-
-$db_select = @mysqli_select_db($conn, $database) or die("Could not select database");
+$db_select = @mysqli_select_db($conn, 'hdt') or die("Could not select database");
 ?>
--- handle_datetime.php
+++ handle_datetime.php
@@ -2,13 +2,15 @@
 require "connect.php";
 
 $date_time = $_POST['h1'];
+$student_id = $_POST['whoami'];
 
-$query = "INSERT INTO appointments (teacher_id, happens_at, flavor, student_id) VALUES ('1', FROM_UNIXTIME($date_time), 'slack', '2')";
+$query = "INSERT INTO appointments (teacher_id, happens_at, flavor, student_id) VALUES ('1', FROM_UNIXTIME(" . $date_time . "), 'slack', '" . $student_id . "')";
 			
 $enter_data = @mysqli_query($conn, $query);
 
-if(!$enter_data){
-	echo "The data could not be sent";
+if($enter_data){
+	header("Location: index.php");
+	die();
 }
 ?>
 
