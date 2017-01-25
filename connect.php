cc3c5ebbdd1d86e18288f90238ef9449bc2381c7
Andrew Arrow <oneone@gmail.com>
2017-01-25 06:30:31 +0000

    getenv for password http://stackoverflow.com/questions/21592832/use-heroku-config-vars-with-php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("138.197.209.226", "root", "root") or die("Could not connect");
+$conn = mysqli_connect("138.197.209.226", "root", getenv('DBPASSWORD')) or die("Could not connect");
 
 $db_select = @mysqli_select_db($conn, 'hdt') or die("Could not select database");
 ?>
